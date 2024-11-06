3593119cf6d84b2d17dc55df4134c5a6d92c4044
lucasmckeon <95000203+lucasmckeon@users.noreply.github.com>
2024-11-06 09:37:22 -1000

    Initial commit

---
+++ index.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+const PORT = 3001;
+const app = express();
+app.use(express.json());
+
+app.get('/ping', (_req, res) => {
+  res.send('pong');
+});
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
